83fe86317f8627a0caa7dc41c29cd4347c2000e4
jeongbin5211 <hjn123493@gmail.com>
2024-01-01 21:40:35 +0900

    0101

--- ex1customer.jpa/customer-exam/src/main/java/io/jeongbin/jpa/CustomerJpaExam.java
+++ ex1customer.jpa/customer-exam/src/main/java/io/jeongbin/jpa/CustomerJpaExam.java
@@ -1,10 +1,9 @@
 package io.jeongbin.jpa;
 
 import io.jeongbin.jpa.entity.Customer;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class CustomerJpaExam {
 
@@ -16,15 +15,29 @@ public class CustomerJpaExam {
         tx.begin();
         //
         try{
-//            em.persist(Customer.sample());
-            Customer foundCustomer = em.find(Customer.class, "ID1001");
+            // em.persist(Customer.sample());
+            //Customer foundCustomer = em.find(Customer.class, "ID1001");
 
-            em.remove(foundCustomer);
             // foundCustomer.setName("Park");
 
-            System.out.println(foundCustomer.toString());
+            // em.remove(foundCustomer);
 
-            tx.commit();
+            // System.out.println(foundCustomer.toString());
+
+            Customer customer = new Customer("ID0005", "Jin"); // 비영속 상태(new)
+            em.persist(customer); // Customer 객체가 영속 상태로 됨 (Managed 상태)
+            em.detach(customer);  // Customer 객체를 준영속 상태로 (Detached 상태)
+            // em.flush(); // 데이터베이스에 반영이 됨 단, 커밋하지 않으면 다른프로그램에서 인식하지 못함
+
+            // JPQL
+//            Query query = em.createQuery("select c from Customer c", Customer.class);
+//            List<Customer> customers = query.getResultList();
+//            System.out.println(customers);
+
+            Customer foundCustomer = em.find(Customer.class, "ID0005");
+            System.out.println(foundCustomer.toString());
+            
+            tx.commit(); // 플러시 또한 같이 진행되므로 플러시를 따로 실행할 필요가 없음
         } catch (Exception e) {
             tx.rollback();
         } finally {
